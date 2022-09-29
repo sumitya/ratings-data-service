65a52df4cb8cd6546f3e9288d7e89bdee3d14b6c
syadav <syadavgit@gmail.com>
2022-09-29 13:43:55 +0530

    Initial commit - ratings-data-service

---
+++ src/main/java/com/example/ratingsdataservice/resource/RatingsResource.java
@@ -0,0 +1,26 @@
+package com.example.ratingsdataservice.resource;
+
+import com.example.ratingsdataservice.model.Rating;
+import com.example.ratingsdataservice.model.UserRating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+    @RequestMapping("/movies/{movieId}")
+    public Rating getMovieRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+        }
+
+    @RequestMapping("/user/{userId}")
+    public UserRating getUserRatings(@PathVariable("userId") String userId) {
+        UserRating userRating = new UserRating();
+        userRating.initData(userId);
+        return userRating;
+
+    }
+}
+
